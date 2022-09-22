5bab1b36a9ec7e719c4df83b39283828ad0ce2de
bnallapeta <bnr@redhat.com>
2022-09-22 16:16:13 +0530

    permission issue fixed

--- scripts/triggergitlabci.sh
+++ scripts/triggergitlabci.sh
@@ -9,7 +9,8 @@ ca_cert=$ca_cert
 
 # decode the value of ca-cert and copy it to the path
 decoded_ca_cert=$(echo "$ca_cert" | base64 -d)
-echo "$decoded_ca_cert" > /etc/ssl/certs/ca-bundle.crt
+echo "$decoded_ca_cert" > /tmp/decoded_ca_cert.crt
+sudo cp /tmp/decoded_ca_cert.crt /etc/ssl/certs/ca-bundle.crt
 
 for i in {1..3}
 do
