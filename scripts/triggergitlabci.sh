17a7b3d4e9b58b5e67cde0f229f7ea911a64f271
bnallapeta <bnr@redhat.com>
2022-09-22 16:08:18 +0530

    Added trigger image and script

---
+++ scripts/triggergitlabci.sh
@@ -0,0 +1,22 @@
+#!/usr/bin/env bash
+
+#quit if exit status of any cmd is a non-zero value
+set -exuo pipefail
+
+# extract values from env variables
+gitlab_token=$gitlab_token
+ca_cert=$ca_cert
+
+# decode the value of ca-cert and copy it to the path
+decoded_ca_cert=$(echo "$ca_cert" | base64 -d)
+echo "$decoded_ca_cert" > /etc/ssl/certs/ca-bundle.crt
+
+for i in {1..3}
+do
+  res=$(curl -s -w '%{http_code}' -X POST --fail -F token="$gitlab_token" -F ref=main https://gitlab.cee.redhat.com/api/v4/projects/65094/trigger/pipeline)
+  if [[ "$res" == 201 ]]; then
+    exit 0
+  fi
+done
+
+exit 1
