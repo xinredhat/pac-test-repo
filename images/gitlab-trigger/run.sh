aa3c20cbf335c0d352ca12572dc122bd65389faa
bnallapeta <bnr@redhat.com>
2022-09-22 16:31:17 +0530

    Added Dockerfile

--- images/gitlab-trigger/run.sh
+++ images/gitlab-trigger/run.sh
@@ -3,18 +3,15 @@
 #quit if exit status of any cmd is a non-zero value
 set -exuo pipefail
 
-# extract values from env variables
-gitlab_token=$gitlab_token
-ca_cert=$ca_cert
-
 # decode the value of ca-cert and copy it to the path
-decoded_ca_cert=$(echo "$ca_cert" | base64 -d)
+decoded_ca_cert=$(echo "$CA_CERT" | base64 -d)
 echo "$decoded_ca_cert" > /tmp/decoded_ca_cert.crt
 sudo cp /tmp/decoded_ca_cert.crt /etc/ssl/certs/ca-bundle.crt
+sudo cat /etc/ssl/certs/ca-bundle.crt
 
-for i in {1..3}
+for _ in {1..3}
 do
-  res=$(curl -s -w '%{http_code}' -X POST --fail -F token="$gitlab_token" -F ref=main https://gitlab.cee.redhat.com/api/v4/projects/65094/trigger/pipeline)
+  res=$(curl -s -w '%{http_code}' -X POST --fail -F token="$GITLAB_TOKEN" -F ref=main https://gitlab.cee.redhat.com/api/v4/projects/65094/trigger/pipeline)
   if [[ "$res" == 201 ]]; then
     exit 0
   fi
