40b190c4e2f1c7404e1571ab24c065c52b24668d
bnallapeta <bnr@redhat.com>
2022-09-23 13:39:37 +0530

    Removed -s

--- images/gitlab-trigger/run.sh
+++ images/gitlab-trigger/run.sh
@@ -5,7 +5,7 @@ set -exuo pipefail
 
 for _ in {1..3}
 do
-  res=$(curl -s -k -w '%{http_code}' -X POST --fail -F token="$GITLAB_TOKEN" -F ref=main https://gitlab.cee.redhat.com/api/v4/projects/65094/trigger/pipeline)
+  res=$(curl -k -w '%{http_code}' -X POST --fail -F token="$GITLAB_TOKEN" -F ref=main https://gitlab.cee.redhat.com/api/v4/projects/65094/trigger/pipeline)
   if [[ "$res" == 201 ]]; then
     exit 0
   fi
